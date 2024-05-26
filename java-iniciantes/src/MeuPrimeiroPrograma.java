3eec4786017a9e7f1cd75a3494d3f865a6012b72
Antonioafj <antonioafj.edu@gmail.com>
2024-05-26 13:58:03 -0400

    java iniciantes
    Signed-off-by: Antonioafj <antonioafj.edu@gmail.com>

---
+++ java-iniciantes/src/MeuPrimeiroPrograma.java
@@ -0,0 +1,10 @@
+public class MeuPrimeiroPrograma {
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("Ola, " + args[0]);
+        System.out.println("Ola, " + args[1]);
+        System.out.println("Ola, " + args[2]);
+        System.out.println("Ola, " + args[3]);
+
+    }
+}
