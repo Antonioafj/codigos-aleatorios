327bb39e28cf89607994f35abaea35d41f1136cc
Antonioafj <antonioafj.edu@gmail.com>
2024-06-02 08:44:39 -0400

    sartetostring

---
+++ sortetostring/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] numbers = new int[]{10, 23, 1, 3, 54, 21, 11, 65, 92, 5, 32, 22};
+
+        Arrays.sort(numbers);
+
+        System.out.println(numbers);
+
+        System.out.println(Arrays.toString(numbers));
+    }
+}
