6b67f2728bc1bd9e287ad2386b9e43305bbadd9c
Antonioafj <antonioafj.edu@gmail.com>
2024-07-05 08:35:41 -0400

    switch

--- condicionais/src/TesteIfElse.java
+++ condicionais/src/TesteIfElse.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 
 public class TesteIfElse {
     public static void main(String[] args) {
-        Scanner teste = new Scanner(System.in);
-        int testescore;
+        Scanner mes = new Scanner(System.in);
+       /*  int testescore;
         char grade;
         int i = 0;
       
@@ -28,6 +28,58 @@ public class TesteIfElse {
         i++;
         }while(i < 4);
         
+        */
+        int  month;;
+        String monthString;
+       do{
+         System.out.println("Digite um numero do mês de 1 e 12: " );
+        month = mes.nextInt();
+        switch (month) {
+            case 1:
+                monthString = "January";
+                break;
+            case 2:
+                monthString = "February";
+                break;
+            case 3:
+                monthString = "Mach";
+                break;
+            case 4:
+                monthString = "April";
+                break;
+            case 5:
+                monthString = "May";
+                break;
+            case 6:
+                monthString = "June";
+                break;
+            case 7:
+                monthString = "July";
+                break;
+            case 8:
+                monthString = "August";
+                break;
+            case 9:
+                monthString = "September";
+                break;
+            case 10:
+                monthString = "Octuber";
+                break;
+                case 11:
+                monthString = "November";
+                break;
+                case 12:
+                monthString = "December";
+                break;
+        
+            default:
+                monthString ="Invalid Month";
+                break;
+
+            }    
+            System.out.println(monthString);
+        }while(month != 0);
+
     }
     
 }
